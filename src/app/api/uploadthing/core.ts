bc340fb2e79dd233eb8c206deadce90fefad03c5
Anil Bishnoi <anilkumarpanwar2929@gmail.com>
2025-02-08 02:19:07 +0530

    Allow files other than images to be uploaded

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -11,7 +11,7 @@ const f = createUploadthing();
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({
-    image: {
+    blob: {
       /**
        * For full list of options and defaults, see the File Route API reference
        * @see https://docs.uploadthing.com/file-routes#route-config
